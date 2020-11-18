7c819abfe421107e73d56746b2238380164125c5
Quackenstien <donaldhawkins422@gmail.com>
2020-11-18 11:34:34 -0700

    styled Hero elements

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Navbar from "../NavBar";
-import { HeroContainer } from "./HeroElements";
+import {
+  HeroContainer,
+  HeroContent,
+  HeroItems,
+  HeroH1,
+  HeroP,
+  HeroBtn,
+} from "./HeroElements";
 
 function Hero() {
   return (
     <HeroContainer>
       <Navbar />
+      <HeroContent>
+        <HeroItems>
+          <HeroH1>Worlds Most Delectable Popcorn</HeroH1>
+          <HeroP>Delivery So Fast The Popcorn Stays Hot</HeroP>
+          <HeroBtn>Place Order</HeroBtn>
+        </HeroItems>
+      </HeroContent>
     </HeroContainer>
   );
 }
