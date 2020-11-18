7cf3837426618dc3c390a2b44bbc1c94e7c6cdf2
Quackenstien <donaldhawkins422@gmail.com>
2020-11-18 15:12:57 -0700

    porducts section built

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Hero from "./components/Hero";
 import { BrowserRouter as Router } from "react-router-dom";
-import { GlobalStyle } from "./globaleStyles";
+import { GlobalStyle } from "./globalStyles";
+import { productData } from "./components/Products/data";
+import Products from "./components/Products";
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading="Choose your favorite" data={productData} />
     </Router>
   );
 }
---
+++ src/components/Products/data.js
@@ -0,0 +1,31 @@
+import butteredPopcorn from "../../images/buttered-popcorn.jpg";
+import caramelPopcorn from "../../images/caramel-popcorn.png";
+import spicyBuffalo from "../../images/spicy-buffalo.jpg";
+
+export const productData = [
+  {
+    img: butteredPopcorn,
+    alt: "Popcorn",
+    name: "Butter Popcorn",
+    desc: "Flavorful and buttery popcorn with light salt",
+    price: "$5.99",
+    button: "Add to Cart",
+  },
+
+  {
+    img: caramelPopcorn,
+    alt: "Popcorn",
+    name: "Caramel Popcorn",
+    desc: "Sweet,salty, and delightfully caramel coated",
+    price: "$7.99",
+    button: "Add to Cart",
+  },
+  {
+    img: spicyBuffalo,
+    alt: "Popcorn",
+    name: "Spicy Buffalo",
+    desc: "A beautiful buffalo flavor. 'Wait, these aren't hot wings?'",
+    price: "$6.99",
+    button: "Add to Cart",
+  },
+];
