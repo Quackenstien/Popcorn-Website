a980d28dcbdf8af4c3e9c24660a91214a4d44d84
Quackenstien <donaldhawkins422@gmail.com>
2020-11-18 15:48:10 -0700

    sweet section added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "./components/Hero";
 import { BrowserRouter as Router } from "react-router-dom";
 import { GlobalStyle } from "./globalStyles";
-import { productData } from "./components/Products/data";
+import { productData, productDataTwo } from "./components/Products/data";
 import Products from "./components/Products";
 import Feature from "./components/Feature";
 
@@ -13,6 +13,7 @@ function App() {
       <Hero />
       <Products heading="Choose your favorite" data={productData} />
       <Feature />
+      <Products heading="Sweet Treats" data={productDataTwo} />
     </Router>
   );
 }
